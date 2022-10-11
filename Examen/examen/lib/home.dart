5def5f2ea44461bd8450b16e1ad4dd9b77b805db
ityoup <lizarraga.juan.1gv@gmail.com>
2022-10-11 08:27:37 -0500

    Se agrego el examen

---
+++ Examen/examen/lib/home.dart
@@ -0,0 +1,72 @@
+ import 'package:flutter/material.dart';
+
+
+ import 'package:flutter/material.dart';class Lista extends StatelessWidget {
+  const Lista({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+// Empieza el primer contenedor 
+const Divider(
+          height: 40,
+          color: Colors.white,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+            color: Colors.black87,
+        ),
+      ),
+      height: 350,
+      child: Text("Pendientes",
+      
+      textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ), 
+      ),
+      const Divider(
+          height: 40,
+          color: Colors.white,
+        ),
+// Empieza el segundo contenedor
+    Container(
+      height: 350,
+      child: Text("Por hacer",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: Colors.black87,
+        ),
+      ),
+        ),
+
+        const Divider(
+          height: 40,
+          color: Colors.white,
+        ),
+// Empieza el tercer contenedor
+Container(
+  height: 350,
+      child: Text("Completadas",
+       textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+           ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: Colors.black87,
+        ),
+      ),
+        ),
+      ],
+      
+    );
+  }
+}
