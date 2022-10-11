f5898c3c11e1d959a27a59a4e8ff1e12379936de
jclizarraga <jclizarraga@velasresorts.com>
2022-10-11 15:14:39 -0500

    RAMA TRABAJO

---
+++ examen2/examen/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:examen/home.dart';
+import 'package:examen/drawer.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+              ), onPressed: () {  },)
+          ],
+          centerTitle: true,
+          title: const Text('Lista de tareas',
+          ),
+        ),
+        body: Lista(),
+        drawer: Back(),
+      ),
+    );
+  }
+}
