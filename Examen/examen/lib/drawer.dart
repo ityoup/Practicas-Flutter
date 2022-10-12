656291bee990bbf7009a27dcdcc77d0535410871
ityoup <lizarraga.juan.1gv@gmail.com>
2022-10-11 19:07:27 -0500

    ACTUALIZACION EXAMEN

--- Examen/examen/lib/drawer.dart
+++ Examen/examen/lib/drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -8,26 +10,159 @@ class Back extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(child: ListView(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black
-            ),
-            borderRadius: BorderRadius.circular(10)
-          ),
-          
-          child: ListTile(
-          title: Text("Categorías",
+        ListTile(
+          title: Text("¿Que tipo de comida quieres?",
           textAlign: TextAlign.center,
           style: TextStyle(
             fontSize: 24,
+            fontWeight: FontWeight.bold
           ),
           ),
         ),
-        )
+
+      const Divider(
+        height: 40,
+      ),
+
+
+
+        ListTile(
+          title: Text("Comida Mexicana",
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.food_bank_rounded),
+            onPressed: (){
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (context) => const Galeria())
+              );
+            }
+          ),
+        ),
+
+        const Divider(
+          height: 20,
+        ),
+
+        ListTile(
+          title: Text("Comida Americana",
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.food_bank_outlined),
+            onPressed: (){
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (context) => const GaleriaAmericana())
+              );
+            },
+          ),
+        ),
+
+        const Divider(
+          height: 20,
+        ),
+
+        ListTile(
+          title: Text("Comida Asiatica",
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.food_bank_rounded),
+            onPressed: () {
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (context) => const GaleriaAsiatica())
+              );
+            },
+          )
+        ),
         
-        
+        const Divider(
+          height: 20,
+        ),
+
       ],
     ),);
   }
 }
+
+//Aqui empieza la pagina 2
+class Galeria extends StatelessWidget {
+  const Galeria({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      child: Text("Pagina para comida mexicana"),
+    );
+  }
+}
+
+
+//Aqui empeiza la comida america
+class GaleriaAmericana extends StatelessWidget {
+  const GaleriaAmericana({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Container(
+          margin: const EdgeInsets.all(30),
+          width: 8.0,
+          height: 300.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: NetworkImage("https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"), 
+              fit: BoxFit.fill
+              ),
+            border: Border.all(
+            color: Colors.black87,    
+        ),
+      ),      
+      ),
+      Container(
+        margin: const EdgeInsets.all(30),
+        width: 8.0,
+        height: 300.0,
+        child: Text("New York",
+        style: TextStyle(fontSize: 14),
+        textAlign: TextAlign.center,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all()
+        ),
+      )
+        
+      ],
+      
+    );
+  }
+}
+
+// Aqui empieza la comida Asiatica
+class GaleriaAsiatica extends StatelessWidget {
+  const GaleriaAsiatica({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Aqui empieza la comida asiatica"),
+    );
+  }
+  
+}
+
--- Examen/examen/lib/home.dart
+++ Examen/examen/lib/home.dart
@@ -16,12 +16,17 @@ const Divider(
         Container(
           width: MediaQuery.of(context).size.width * 0.50,
           decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"), 
+              fit: BoxFit.fill
+              ),
             border: Border.all(
             color: Colors.black87,
+            
         ),
       ),
       height: 350,
-      child: Text("Hoy",
+      child: Text("Platillo del día",
       textAlign: TextAlign.left,
       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
       ),
@@ -34,11 +39,15 @@ const Divider(
 // Empieza el segundo contenedor
     Container(
       height: 350,
-      child: Text("Por hacer",
+      child: Text("A nuestra seleccion",
       textAlign: TextAlign.left,
       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
       ),
           decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"), 
+              fit: BoxFit.fill
+            ),
             // borderRadius: BorderRadius.circular(20),
             border: Border.all(
               color: Colors.black87,
@@ -53,18 +62,61 @@ const Divider(
 // Empieza el tercer contenedor
 Container(
   height: 350,
-      child: Text("Completadas",
+      child: Text("Para los niños",
        textAlign: TextAlign.left,
       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
            ),
           decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("https://images.unsplash.com/photo-1603573561692-d36965bd5096?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1459&q=80"),
+              fit: BoxFit.fill
+               ),
             // borderRadius: BorderRadius.circular(10),
             border: Border.all(
               color: Colors.black87,
         ),
       ),
         ),
+      const Divider(
+        height: 40,
+        color: Colors.white,
+      ),
+
+      Container(
+        height: 350,
+        child: Text("Aqui ira otro container",
+        textAlign: TextAlign.left,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage("https://images.unsplash.com/photo-1615870216519-2f9fa575fa5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"),
+            fit: BoxFit.fill 
+            )
+        ),
+      ),
+
+      const Divider(
+        height: 40,
+        color: Colors.white,
+      ),
+      
+    Container(
+      height: 350,
+      child: Text("Hamburguesas",
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage("https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"),
+          fit: BoxFit.fill,
+          
+        )
+      ),
+    ),
       ],
+      
+
       
     );
   }
